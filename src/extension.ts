774a0aa9a19631e6c9e071797e4cb718fc165d67
Hiromi ISHII <konn.jinro@gmail.com>
2018-11-11 21:54:09 +0900

    Makes the extension more generic and extensible input method

--- src/extension.ts
+++ src/extension.ts
@@ -1,199 +1,90 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import {
-    languages, workspace, window, SnippetString,
-    CompletionItem, ExtensionContext,
-    WorkspaceConfiguration, QuickPickItem,
-    TextDocument, Position, Range, TextEdit,
-    CancellationToken, CompletionContext, CompletionTriggerKind, commands
-} from 'vscode';
+  CancellationToken,
+  commands,
+  CompletionContext,
+  CompletionTriggerKind,
+  ExtensionContext,
+  languages,
+  Position,
+  Range,
+  TextDocument,
+  TextEdit,
+  window,
+  workspace,
+  WorkspaceConfiguration
+} from "vscode";
+import InputMethod, { InputMethodConf } from "./input_method";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "latex-shape-completion" is now active!');
-    let conf: WorkspaceConfiguration = workspace.getConfiguration();
+  let conf: WorkspaceConfiguration = workspace.getConfiguration();
 
-    function register_completer(cmd_name: string, dictionary: string, ...keys: string[]) {
-        const dict: ShapeCompletionItemIF[] = conf.get(dictionary) || [];
-        let disposable = languages.registerCompletionItemProvider("latex", {
-            provideCompletionItems(
-                document: TextDocument,
-                position: Position,
-                token: CancellationToken,
-                context: CompletionContext
+  let inputMethods: InputMethodConf[] = conf.get("input-methods", []);
+  inputMethods.forEach(imConf => {
+    const im = new InputMethod(context, imConf);
+    const dict = im.completionItems();
+    im.languages.forEach(lang => {
+      let compProvider = languages.registerCompletionItemProvider(
+        lang,
+        {
+          provideCompletionItems(
+            document: TextDocument,
+            position: Position,
+            token: CancellationToken,
+            context: CompletionContext
+          ) {
+            if (
+              context.triggerKind === CompletionTriggerKind.TriggerCharacter &&
+              im.triggers.some(c => c === context.triggerCharacter)
             ) {
-                if (context.triggerKind === CompletionTriggerKind.TriggerCharacter &&
-                    keys.some(c => c === context.triggerCharacter)) {
-                    return dict.map(i => {
-                        let item = new ShapeCompletionItem(i);
-                        let start = position;
-                        if (position.character > 0) {
-                            start = new Position(position.line, position.character - 1);
-                        }
-                        let range = new Range(start, position);
-                        item.additionalTextEdits = [TextEdit.delete(range)];
-                        return item;
-                    });
-                } else {
-                    return [];
+              return dict.map(item => {
+                let start = position;
+                if (position.character > 0) {
+                  start = new Position(position.line, position.character - 1);
                 }
+                let range = new Range(start, position);
+                if (document.getText(range) === context.triggerCharacter) {
+                  item.additionalTextEdits = [TextEdit.delete(range)];
+                }
+                return item;
+              });
+            } else {
+              return [];
             }
-        }, ...keys);
-        context.subscriptions.push(disposable);
-    }
-
-    register_completer("greek-complete", "greek-dictionary", ":");
-    register_completer("image-complete", "image-dictionary", ";");
-    register_completer("font-complete", "font-dictionary", "@");
-
-    function register_command(cmd_name: string, dictionary: string) {
-        let dict: ShapeCompletionItem[] = conf.get(dictionary) || [];
-        let disposable = commands.registerCommand(`extension.${cmd_name}`, () => {
-            // The code you place here will be executed every time your command is executed
-            let editor = window.activeTextEditor;
-
-            // Display a message box to the user
-            window.showQuickPick(dict).then(item => {
-                if (!item) {
-                    return;
-                } else {
-                    if (!editor) {
-                        return;
-                    }
-                    console.log(`Selected: ${JSON.stringify(item)}`);
-                    editor.insertSnippet((new ShapeCompletionItem(item).render()));
-                }
+          }
+        },
+        ...im.triggers
+      );
+      context.subscriptions.push(compProvider);
+    });
+    let cmd_name = im.commandName;
+    if (cmd_name) {
+      const picks = im.quickPickItems();
+      let pickCommand = commands.registerTextEditorCommand(
+        `extension.complete.${cmd_name}`,
+        (editor, _edit) => {
+          if (im.languages.some(i => i === editor.document.languageId)) {
+            let selection: string | undefined;
+            if (!editor.selection.isEmpty) {
+              selection = editor.document.getText(editor.selection);
+            }
+            window.showQuickPick(picks).then(item => {
+              if (!item) {
+                return;
+              }
+              editor.insertSnippet(item.toSnippet(selection));
             });
-        });
-        context.subscriptions.push(disposable);
+          }
+        }
+      );
+      context.subscriptions.push(pickCommand);
     }
-
-    register_command("greek-complete", "greek-dictionary");
-    register_command("image-complete", "image-dictionary");
-    register_command("font-complete", "font-dictionary");
+  });
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
-
-enum CommandType {
-    Maketitle = "maketitle",
-    Environment = "environment",
-    Section = "section",
-    Text = "text",
-    Large = "large"
-}
-
-interface ArgSpec {
-    kind: ArgKind; candidates?: string[];
-}
-
-/**
- * render_argspec
- */
-function render_argspec(selection: string): (spec: ArgSpec, i: number) => string {
-    return function (value: ArgSpec, index: number): string {
-        let rendered = "";
-        let cands = value.candidates;
-        if (cands) {
-            rendered = `\${${index}|${cands.join(",")}|}`;
-        } else if (selection.length > 0 && index === 0) {
-            rendered = selection;
-        } else {
-            rendered = `\${${index}}`;
-        }
-        switch (value.kind) {
-            case ArgKind.Fixed:
-                rendered = `{${rendered}}`;
-            case ArgKind.Optional:
-                rendered = `[${rendered}]`;
-        }
-        return rendered;
-    };
-}
-
-enum ArgKind { Fixed = "fixed", Optional = "optional" }
-
-interface ShapeCompletionItemIF {
-    label: string;
-    body: string;
-    filterText: string;
-    description: string;
-    type?: CommandType;
-    args?: ArgSpec[];
-}
-
-class ShapeCompletionItem implements QuickPickItem, CompletionItem {
-    public insertText: SnippetString;
-    public documentation: string;
-    public label: string;
-    public body: string;
-    public filterText: string;
-    public description: string;
-    public type?: CommandType;
-    public args?: ArgSpec[];
-    public additionalTextEdits?: TextEdit[];
-    public commitCharacters?: string[];
-    constructor(
-        item: ShapeCompletionItemIF
-    ) {
-        this.label = item.label;
-        this.body = item.body;
-        this.filterText = item.filterText;
-        this.description = item.description;
-        this.type = item.type;
-        this.args = item.args;
-        this.insertText = this.render();
-        this.documentation = this.description;
-        this.commitCharacters = [" "];
-    }
-
-    /**
-     * render
-     */
-    public render() {
-        let rendered = "";
-        let editor = window.activeTextEditor;
-        let selection = "";
-        if (editor) {
-            selection = editor.document.getText(editor.selection);
-        }
-        let args = (this.args || []).map(render_argspec(selection)).join("");
-
-        switch (this.type) {
-            case CommandType.Environment:
-                rendered = `\\begin{${this.body}}${args}
-          $1
-        \\end{${this.body}}`;
-                break;
-            case CommandType.Large:
-                rendered = `{\\${this.body} $1}`;
-                break;
-            case CommandType.Section:
-                if (!this.args || this.args.length === 0) {
-                    if (selection.length === 0) {
-                        rendered = `\\${this.body}{$1}`;
-                    } else {
-                        rendered = `\\${this.body}{${selection}}`;
-                    }
-                } else {
-                    rendered = `\\${this.body}${args}`;
-                }
-                break;
-            case CommandType.Text:
-                rendered = this.body;
-                break;
-
-            default:
-                rendered = `\\${this.body}`;
-        }
-        return new SnippetString(rendered);
-    }
-
-}
-
+export function deactivate() {}
---
+++ src/input_method.ts
@@ -0,0 +1,255 @@
+import {
+  CompletionItem,
+  ExtensionContext,
+  QuickPickItem,
+  SnippetString
+} from "vscode";
+import { readFileSync } from "fs";
+
+const CHAR_SPACE: number = 32;
+const CHAR_TILDE: number = 126;
+const ASCII_CHARS: string[] = Array(CHAR_TILDE - CHAR_SPACE + 1)
+  .fill(0)
+  .map((_, offset) => String.fromCharCode(offset + CHAR_SPACE));
+
+export interface ToSnippet {
+  toSnippet(selection?: string): SnippetString;
+}
+
+export enum RenderMode {
+  String = "string",
+  SnippetString = "snippet",
+  LaTeXCommand = "latex"
+}
+
+export interface InputMethodConf {
+  name: string;
+  languages: string[];
+  triggers: string[];
+  dictionary: (InputMethodItemConfig | string)[] | string;
+  renderMode?: RenderMode;
+  commandName?: string;
+}
+
+export default class InputMethod {
+  public name: string;
+  public languages: string[];
+  public triggers: string[];
+  public renderMode: RenderMode;
+  public commandName?: string;
+
+  private dictionary: InputMethodItem[];
+
+  constructor(context: ExtensionContext, conf: InputMethodConf) {
+    this.name = conf.name;
+    this.languages = conf.languages;
+    this.triggers = conf.triggers;
+    this.dictionary = [];
+    this.renderMode = conf.renderMode ? conf.renderMode : RenderMode.String;
+    this.commandName = conf.commandName;
+
+    function parseFile(path: string) {
+      return JSON.parse(readFileSync(context.asAbsolutePath(path)).toString());
+    }
+
+    const dictSeed = conf.dictionary;
+    let dict: InputMethodItemConfig[] = [];
+    if (typeof dictSeed === "string") {
+      dict = parseFile(dictSeed);
+    } else {
+      dictSeed.forEach(i => {
+        if (typeof i === "string") {
+          dict = dict.concat(parseFile(i));
+        } else {
+          dict.push(i);
+        }
+      });
+    }
+
+    switch (this.renderMode) {
+      case RenderMode.LaTeXCommand:
+        dict.forEach(i => {
+          this.dictionary.push(
+            new LaTeXInputMethodItem(i as LaTeXInputMethodItemConfig)
+          );
+        });
+        break;
+
+      default:
+        dict.forEach(i => {
+          this.dictionary.push(new SimpleInputMethodItem(i));
+        });
+    }
+  }
+
+  /**
+   * getCompletionItems
+   */
+  public completionItems(): CompletionItem[] {
+    const commiters = ASCII_CHARS.filter(
+      c => !this.dictionary.some(i => i.label.indexOf(c) !== -1)
+    );
+    return this.dictionary.map(i => {
+      let ci: CompletionItem = {
+        label: i.label,
+        insertText: i.toSnippet(),
+        filterText: i.label,
+        documentation: i.description,
+        commitCharacters: commiters
+      };
+      return ci;
+    });
+  }
+
+  /**
+   * quickPickItems
+   */
+  public quickPickItems(): RenderableQuickPickItem[] {
+    return this.dictionary.map(i => {
+      return {
+        description: i.description,
+        label: i.label,
+        toSnippet: (e?: string) => i.toSnippet(e)
+      };
+    });
+  }
+}
+
+export interface RenderableQuickPickItem extends QuickPickItem, ToSnippet {}
+
+export class SimpleInputMethodItem implements ToSnippet {
+  public label: string;
+  public body: string;
+  public description?: string;
+  constructor(i: InputMethodItemConfig) {
+    this.label = i.label;
+    this.body = i.body;
+    this.description = i.description;
+  }
+
+  /**
+   * toSnippet
+   */
+  public toSnippet(_?: string): SnippetString {
+    return new SnippetString(this.body);
+  }
+}
+
+export interface InputMethodItem extends ToSnippet {
+  label: string;
+  body: string;
+  description?: string;
+}
+
+export interface InputMethodItemConfig {
+  label: string;
+  body: string;
+  description?: string;
+  [index: string]: any;
+}
+
+enum CommandType {
+  Maketitle = "maketitle",
+  Environment = "environment",
+  Section = "section",
+  Text = "text",
+  Large = "large"
+}
+
+interface ArgSpec {
+  kind: ArgKind;
+  candidates?: string[];
+}
+
+/**
+ * render_argspec
+ */
+function render_argspec(
+  selection: string
+): (spec: ArgSpec, i: number) => string {
+  return function(value: ArgSpec, index: number): string {
+    let rendered = "";
+    let cands = value.candidates;
+    if (cands) {
+      rendered = `\${${index}|${cands.join(",")}|}`;
+    } else if (selection.length > 0 && index === 0) {
+      rendered = selection;
+    } else {
+      rendered = `\${${index}}`;
+    }
+    switch (value.kind) {
+      case ArgKind.Fixed:
+        rendered = `{${rendered}}`;
+      case ArgKind.Optional:
+        rendered = `[${rendered}]`;
+    }
+    return rendered;
+  };
+}
+
+export enum ArgKind {
+  Fixed = "fixed",
+  Optional = "optional"
+}
+
+export interface LaTeXInputMethodItemConfig {
+  label: string;
+  body: string;
+  filterText: string;
+  description: string;
+  type?: CommandType;
+  args?: ArgSpec[];
+}
+
+export class LaTeXInputMethodItem implements InputMethodItem {
+  public label: string;
+  public body: string;
+  public description: string;
+  public type?: CommandType;
+  public args?: ArgSpec[];
+  constructor(item: LaTeXInputMethodItemConfig) {
+    this.label = item.label;
+    this.body = item.body;
+    this.description = item.description;
+    this.type = item.type;
+    this.args = item.args;
+  }
+
+  /**
+   * render
+   */
+  public toSnippet(selection: string = ""): SnippetString {
+    let rendered = "";
+
+    let args = (this.args || []).map(render_argspec(selection)).join("");
+
+    switch (this.type) {
+      case CommandType.Environment:
+        rendered = `\\begin{${this.body}}${args}
+  $1
+\\end{${this.body}}`;
+        break;
+      case CommandType.Large:
+        rendered = `{\\${this.body} $1}`;
+        break;
+      case CommandType.Section:
+        if (!this.args || this.args.length === 0) {
+          if (selection.length === 0) {
+            rendered = `\\${this.body}{$1}`;
+          } else {
+            rendered = `\\${this.body}{${selection}}`;
+          }
+        } else {
+          rendered = `\\${this.body}${args}`;
+        }
+        break;
+      case CommandType.Text:
+        rendered = this.body;
+        break;
+
+      default:
+        rendered = `\\${this.body}`;
+    }
+    return new SnippetString(rendered);
+  }
+}
